9e52761e394e9d19f2b1dc42ebc7a795c7f1fea6
simon <sione.brusa1234@gmail.com>
2025-10-27 09:38:18 +0100

    cambio di pagine e aggiunta di homepage

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,28 @@
 import { Component, signal, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import {Navbar} from './navbar/navbar';
+import {Navbar2} from './navbar2/navbar2';
 import {RouterModule, RouterOutlet} from '@angular/router';
 import {PartitaViewComponent} from './component/partita-view/partita-view';
-import {Navbar2} from './navbar2/navbar2';
-import {ChessBoardComponent}  from './component/chess-board/chess-board';
+import {NgIf} from '@angular/common';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Navbar, RouterOutlet, RouterModule, PartitaViewComponent, Navbar2],
+  imports: [Navbar, RouterOutlet, RouterModule, PartitaViewComponent, Navbar2, NgIf],
   templateUrl: './app.html',
   styleUrls: ['./app.css'],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppComponent {
   protected readonly title = signal('CheckMateFE');
-  chessBoard: any;
+  loading = true;
+
+  ngOnInit() {
+    // Simula il caricamento del sito (puoi sostituire con reali chiamate HTTP)
+    setTimeout(() => {
+      this.loading = false;
+    }, 1500); // 1.5 secondi, cambia a piacere
+  }
 }
--- src/app/navbar2/navbar2.ts
+++ src/app/navbar2/navbar2.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-navbar2',
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './navbar2.html',
   styleUrl: './navbar2.css'
 })
